b989c2ceced7230dfbcc5be7244aad07e4f704ec
jeongmin <yesuel1111@naver.com>
2023-05-02 14:26:33 +0900

    [jeongmin] BOJ_붙임성 좋은 총총이

--- BOJ/26069.붙임성_좋은_총총이/jeongmin.py
+++ BOJ/26069.붙임성_좋은_총총이/jeongmin.py
@@ -5,12 +5,12 @@ from collections import defaultdict
 input = sys.stdin.readline
 
 N = int(input())
-people = defaultdict(int)
-people["ChongChong"] = 1
+people = defaultdict(bool)
+people["ChongChong"] = True # 춤을 추고 있는 상태
 
 for _ in range(N):
     nameA, nameB = input().split()
-    if people[nameA] == 1 or people[nameB] == 1:
-        people[nameB] = people[nameA] = 1
- 
-print(list(people.values()).count(1))
+    if people[nameA] is True or people[nameB] is True:
+        people[nameB] = people[nameA] = True
+
+print(list(people.values()).count(True))
