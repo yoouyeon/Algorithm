3d6589afc9e026677dc69bae45adc17853219043
jeongmin <yesuel1111@naver.com>
2023-05-03 21:50:38 +0900

    [jeongmin] BOJ_십자카드 문제

--- BOJ/2659.십자카드_문제/jeongmin.py
+++ BOJ/2659.십자카드_문제/jeongmin.py
@@ -1,29 +1,19 @@
-from itertools import product
 from collections import deque
 
-def make_num_case(card):
+def get_clock_num(card):
     case = set()
     queue = deque(card)
     for i in range(4):
         case.add(int(''.join(queue)))
         queue.rotate()
-    return case
 
-input_card = list(input().split())
-total_case = make_num_case(input_card)
-clock_num = min(total_case)
+    return min(case)
 
-total_card = product([i for i in range(1, 10)], repeat = 4)
-
+input_card = get_clock_num(list(input().split()))
 cnt = 1
-for card in total_card:
-    card = list(map(str, card))
-    card_num = int(''.join(card))
-    if clock_num == card_num:
-        break
-    if card_num in total_case:
-        continue
-    total_case |= make_num_case(card)
-    cnt += 1
+
+for i in range(1111, input_card):
+    if get_clock_num(list(str(i))) == i:
+        cnt += 1
 
 print(cnt)
