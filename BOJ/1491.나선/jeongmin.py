146087978f4330c0dd991a7f301345012d0744d1
jeongmin <yesuel1111@naver.com>
2023-05-07 17:26:22 +0900

    [jeongmin] BOJ_나선

---
+++ BOJ/1491.나선/jeongmin.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+pos_x, pos_y = -1, 0 # 시작 위치
+move = 1 # 방향
+
+while N and M:
+    M -= 1
+    pos_x += move * Nㄴ
+    pos_y += move * M
+    move *= -1
+    N -= 1
+
+print(pos_x, pos_y)
