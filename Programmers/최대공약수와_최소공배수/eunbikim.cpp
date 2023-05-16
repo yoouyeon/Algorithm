248494d74abcd51073d4a8e52c05c5341a699ccb
eunbi9n <gombi9_9@naver.com>
2023-05-16 21:26:47 +0900

    [eunbikim] Programmers_최대공약수와 최소공배수

---
+++ Programmers/최대공약수와_최소공배수/eunbikim.cpp
@@ -0,0 +1,36 @@
+// int getLCM(int large, int small) {
+//     for (int i = 1; i < INT_MAX / large; i++) {
+//         if ((large * i) % small == 0)
+//             return large * i;
+//     }
+// }
+
+#include <vector>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+int getGCD(int n, int m) {
+    vector<int> divisor;
+    int max = 1;
+    if (n > m)
+        swap(n, m);
+    for (int i = 1; i <= sqrt(n) ; i++) {
+        if (n % i == 0) {
+            if (m % i == 0 && max < i)
+                max = i;
+            if (i < (n / i) && m % (n / i) == 0 && max < (n / i))
+                max = n / i;
+        } 
+    }
+    return max;
+}
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    int gcd = getGCD(n, m);
+    answer.push_back(gcd);
+    answer.push_back(n * m / gcd);
+    return answer;
+}
