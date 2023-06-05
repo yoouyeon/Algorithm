74f85e1bef96f107f00fb71753f9f0fdfb5263aa
soong-e <yujelee@student.42seoul.kr>
2023-06-05 15:09:08 +0900

    [yujelee] Leetcode_isomorpic Strings

--- LeetCode/205. Isomorphic_Strings/yujelee.py
+++ LeetCode/205. Isomorphic_Strings/yujelee.py
@@ -15,3 +15,15 @@ class Solution:
         
         return True if result == t else False
 
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        m = dict()
+
+        for c1, c2 in zip(s, t):
+            if c1 not in m and c2 not in m.values():
+                m[c1] = c2
+                continue
+            if c1 not in m or m[c1] != c2:
+                return False
+
+        return True
