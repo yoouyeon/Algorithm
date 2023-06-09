bccce9413c8a50b3d8cb67f0643771557d3b7311
srngch <srngch.dev@gmail.com>
2023-06-10 02:00:11 +0900

    [sarchoi] LeetCode_21.Merge Two Sorted Lists - update

--- LeetCode/21.Merge_Two_Sorted_Lists/sarchoi.js
+++ LeetCode/21.Merge_Two_Sorted_Lists/sarchoi.js
@@ -11,9 +11,10 @@
  * @return {ListNode}
  */
 const mergeTwoLists = function(list1, list2) {
-    let head = new ListNode(0, null);
+    let head = new ListNode();
     let p = head;
-    while (list1 !== null && list2 !== null) {
+
+    while (list1 && list2) {
         if (list1.val >= list2.val) {
             p.next = list2;
             list2 = list2.next;
@@ -23,10 +24,7 @@ const mergeTwoLists = function(list1, list2) {
         }
         p = p.next;
     }
+		p.next = list1 || list2;
 
-    if (list1) p.next = list1;
-    if (list2) p.next = list2;
-
-    head = head.next;
-    return head;
+    return head.next;
 };
