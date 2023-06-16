184ca747a96db7a027a7ec03833adff806632841
highcho <highjcho@gmail.com>
2023-06-16 15:26:31 +0900

    [hyunjcho] Leetcode_Maximal Square

---
+++ LeetCode/221.Maximal_Square/hyunjcho.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximalSquare(self, matrix: list[list[str]]) -> int:
+        ans = 0
+        row = len(matrix)
+        col = len(matrix[0])
+        dp = [[0 for _ in range(col + 1)] for _ in range(row + 1)]
+        for i in range(row):
+            for j in range(col):
+                if matrix[i][j] == "1":
+                    dp[i + 1][j + 1] = min(dp[i][j], dp[i + 1][j], dp[i][j + 1]) + 1
+                    ans = max(ans, dp[i + 1][j + 1])
+        return ans * ans
