f77580044a4dea55b1c70547c51e413ea215524a
eunbi9n <eunbikim@student.42seoul.kr>
2023-07-03 10:53:11 +0900

    [eunbikim] Programmers_피보나치 수

---
+++ Programmers/피보나치_수/eunbikim.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+
+int fibo[100001];
+
+int solution(int n) {
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for (int i = 2; i <= n; i++)
+        fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
+    return fibo[n] % 1234567;
+}
